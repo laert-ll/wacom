b4c88b8b6113c0981f43ed03cd2dfac30d8911bf
Laert Llaveshi <l.llaveshi@tum.de>
2021-11-25 00:09:32 +0100

    added first scripts

---
+++ wacom_DP1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xsetwacom --set "Wacom One by Wacom M Pen stylus" MapToOutput DP-1 && xsetwacom set "Wacom One by Wacom M Pen stylus" Button 2 key PgUp && xsetwacom set "Wacom One by Wacom M Pen stylus" Button 3 key PgDn
---
+++ wacom_eDP-1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xsetwacom --set "Wacom One by Wacom M Pen stylus" MapToOutput eDP-1 && xsetwacom set "Wacom One by Wacom M Pen stylus" Button 2 key PgUp && xsetwacom set "Wacom One by Wacom M Pen stylus" Button 3 key PgDn
